030ff2b33a807af68a33fd2a9f76e894770fef8f
Timothy Alcaide <timothy.alcaide@gmail.com>
2023-02-28 20:22:56 +0100

    build: update packages with nx migration

--- apps/options/jest.config.ts
+++ apps/options/jest.config.ts
@@ -1,16 +1,18 @@
-module.exports = {
-  displayName: 'popup',
+/* eslint-disable */
+export default {
+  displayName: 'options',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: '<rootDir>/tsconfig.spec.json',
-      stringifyContentPathRegex: '\\.(html|svg)$',
-    },
-  },
-  coverageDirectory: '../../coverage/apps/popup',
+  globals: {},
+  coverageDirectory: '../../coverage/apps/options',
   transform: {
-    '^.+\\.(ts|mjs|js|html)$': 'jest-preset-angular',
+    '^.+\\.(ts|mjs|js|html)$': [
+      'jest-preset-angular',
+      {
+        tsconfig: '<rootDir>/tsconfig.spec.json',
+        stringifyContentPathRegex: '\\.(html|svg)$',
+      },
+    ],
   },
   transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   snapshotSerializers: [
