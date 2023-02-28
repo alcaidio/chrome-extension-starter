95b086428b3e9526d5731b78b02d7a03a5d80711
Timothy Alcaide <timothy.alcaide@gmail.com>
2023-02-28 21:29:54 +0100

    feat: add chrom focus mode fonctionalities

---
+++ apps/scripts/content/src/main.ts
@@ -0,0 +1,12 @@
+// if (document) {
+//   const text = document.textContent?.split(' ') || [];
+//   const wordCount = text.length;
+//   const readingTime = Math.round(wordCount / 200);
+//   const badge = document.createElement('p');
+//   badge.textContent = `⏱️ ${readingTime} min read`;
+
+//   const heading = document.querySelector('h1');
+
+//   heading?.insertAdjacentElement('afterend', badge);
+//   console.log('from content script:', badge);
+// }
